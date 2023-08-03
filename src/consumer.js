0ddf59d6ee5a1063a2e00c381729a018c60c9057
Redi Ahmad <redi.ngoding@gmail.com>
2023-08-03 10:24:35 +0700

    refactor(consumer): :recycle: add arguments to `listener` instance

--- src/consumer.js
+++ src/consumer.js
@@ -6,7 +6,7 @@ const Listener = require('./Listener');
 const init = async () => {
   const playlistsService = new PlaylistsService();
   const mailSender = new MailSender();
-  const listener = new Listener();
+  const listener = new Listener(playlistsService, mailSender);
 
   const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
