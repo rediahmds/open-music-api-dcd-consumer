e94fab6d359353a3444f1c07451cc0557fc2a748
Redi Ahmad <redi.ngoding@gmail.com>
2023-08-03 21:38:33 +0700

    feat(service): :sparkles: implements `TracksService` in consumer

--- src/consumer.js
+++ src/consumer.js
@@ -1,12 +1,14 @@
 const amqp = require('amqplib');
 const PlaylistsService = require('./PlaylistsService');
+const TracksService = require('./TracksService');
 const MailSender = require('./MailSender');
 const Listener = require('./Listener');
 
 const init = async () => {
   const playlistsService = new PlaylistsService();
+  const tracksService = new TracksService();
   const mailSender = new MailSender();
-  const listener = new Listener(playlistsService, mailSender);
+  const listener = new Listener(playlistsService, tracksService, mailSender);
 
   const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
   const channel = await connection.createChannel();
